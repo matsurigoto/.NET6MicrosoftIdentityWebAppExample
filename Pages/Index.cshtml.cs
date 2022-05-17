818429356cb21b43aae94a2bc8b88f44a4464baa
Duran Hsieh <dog0416@gmail.com>
2022-05-17 15:19:45 +0800

    init

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+
+namespace my_app.Pages;
+
+[AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    private readonly GraphServiceClient _graphServiceClient;
+
+    public IndexModel(ILogger<IndexModel> logger,
+                        GraphServiceClient graphServiceClient)
+    {
+        _logger = logger;
+        _graphServiceClient = graphServiceClient;
+    }
+
+    public async Task OnGet()
+    {
+        var user = await _graphServiceClient.Me.Request().GetAsync();
+
+        ViewData["ApiResult"] = user.DisplayName;
+    }
+}
